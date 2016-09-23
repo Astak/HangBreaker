4c713bbbeb3c3b4505da11be593e7e57e8f7fac3
Astak <uranus@xaker.ru>
2016-09-23 23:22:18 +0300

    Implemented the Start command

---
+++ HangBreaker.Tests/MainViewModelTests.cs
@@ -0,0 +1,29 @@
+﻿using DevExpress.Mvvm.POCO;
+using HangBreaker.ViewModels;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HangBreaker.Tests {
+    [TestClass]
+    public class MainViewModelTests {
+        [TestMethod]
+        public void StartActionTest() {
+            var viewModel = ViewModelSource.Create<MainViewModel>();
+            Assert.IsTrue(viewModel.CanStart());
+            Assert.IsFalse(viewModel.CanRestart());
+            viewModel.Start();
+            Assert.IsFalse(viewModel.CanStart());
+            Assert.IsTrue(viewModel.CanRestart());
+            var interval = 5 * 60;
+            for (int i = 0; i < interval; i++) viewModel.Tick();
+            Assert.IsTrue(viewModel.CanStart());
+            Assert.IsTrue(viewModel.CanRestart());
+            viewModel.Start();
+            Assert.IsFalse(viewModel.CanStart());
+            Assert.IsTrue(viewModel.CanRestart());
+            interval = 10 * 60;
+            for (int i = 0; i < interval; i++) viewModel.Tick();
+            Assert.IsFalse(viewModel.CanStart());
+            Assert.IsTrue(viewModel.CanRestart());
+        }
+    }
+}
---
+++ HangBreaker/ViewModels/MainViewModel.cs
@@ -0,0 +1,41 @@
+﻿namespace HangBreaker.ViewModels {
+    public class MainViewModel {
+        private ViewModelState State;
+        private int Elapsed;
+
+        public bool CanStart() {
+            return State == ViewModelState.Initial || State == ViewModelState.PreviewOverflow;
+        }
+
+        public bool CanRestart() {
+            return State == ViewModelState.Preview || State == ViewModelState.PreviewOverflow || 
+                State == ViewModelState.Work || State == ViewModelState.WorkOverflow;
+        }
+
+        public void Start() {
+            switch (State) {
+                case ViewModelState.Initial:
+                    State = ViewModelState.Preview;
+                    break;
+                case ViewModelState.PreviewOverflow:
+                    State = ViewModelState.Work;
+                    break;
+            }
+        }
+
+        private enum ViewModelState { Initial, Preview, PreviewOverflow, Work, WorkOverflow }
+
+        public void Tick() {
+            if (!(State == ViewModelState.Preview || State == ViewModelState.Work)) return;
+            if (--Elapsed > 0) return;
+            switch (State) {
+                case ViewModelState.Preview:
+                    State = ViewModelState.PreviewOverflow;
+                    break;
+                case ViewModelState.Work:
+                    State = ViewModelState.WorkOverflow;
+                    break;
+            }
+        }
+    }
+}
