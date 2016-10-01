4209f2a8df667de012a256787234428dc04d9679
Astak <uranus@xaker.ru>
2016-10-01 21:08:12 +0300

    Reduced the window size

--- HangBreaker/MainForm.Designer.cs
+++ HangBreaker/MainForm.Designer.cs
@@ -24,19 +24,58 @@
         /// </summary>
         private void InitializeComponent() {
             this.components = new System.ComponentModel.Container();
-            HangBreaker.Controls.Button ButtonRestart;
-            HangBreaker.Controls.Button ButtonStart;
             System.Windows.Forms.Label LabelDisplay;
             System.Windows.Forms.Button ButtonExit;
+            HangBreaker.Controls.Button ButtonRestart;
+            HangBreaker.Controls.Button ButtonStart;
             HangBreaker.Controls.Timer Timer;
             this.MvvmContext = new DevExpress.Utils.MVVM.MVVMContext(this.components);
-            ButtonRestart = new HangBreaker.Controls.Button();
-            ButtonStart = new HangBreaker.Controls.Button();
             LabelDisplay = new System.Windows.Forms.Label();
             ButtonExit = new System.Windows.Forms.Button();
+            ButtonRestart = new HangBreaker.Controls.Button();
+            ButtonStart = new HangBreaker.Controls.Button();
             Timer = new HangBreaker.Controls.Timer();
             ((System.ComponentModel.ISupportInitialize)(this.MvvmContext)).BeginInit();
             this.SuspendLayout();
+            // 
+            // LabelDisplay
+            // 
+            LabelDisplay.Dock = System.Windows.Forms.DockStyle.Fill;
+            LabelDisplay.Font = new System.Drawing.Font("Comic Sans MS", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            LabelDisplay.ForeColor = System.Drawing.Color.Red;
+            LabelDisplay.Location = new System.Drawing.Point(0, 0);
+            LabelDisplay.Name = "LabelDisplay";
+            LabelDisplay.Size = new System.Drawing.Size(319, 90);
+            LabelDisplay.TabIndex = 0;
+            LabelDisplay.Text = "Overtime";
+            LabelDisplay.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // ButtonExit
+            // 
+            ButtonExit.Location = new System.Drawing.Point(275, 12);
+            ButtonExit.Name = "ButtonExit";
+            ButtonExit.Size = new System.Drawing.Size(32, 32);
+            ButtonExit.TabIndex = 3;
+            ButtonExit.UseVisualStyleBackColor = true;
+            ButtonExit.Visible = false;
+            // 
+            // ButtonRestart
+            // 
+            ButtonRestart.Location = new System.Drawing.Point(50, 46);
+            ButtonRestart.Name = "ButtonRestart";
+            ButtonRestart.Size = new System.Drawing.Size(32, 32);
+            ButtonRestart.TabIndex = 2;
+            ButtonRestart.UseVisualStyleBackColor = true;
+            ButtonRestart.Visible = false;
+            // 
+            // ButtonStart
+            // 
+            ButtonStart.Location = new System.Drawing.Point(12, 46);
+            ButtonStart.Name = "ButtonStart";
+            ButtonStart.Size = new System.Drawing.Size(32, 32);
+            ButtonStart.TabIndex = 1;
+            ButtonStart.UseVisualStyleBackColor = true;
+            ButtonStart.Visible = false;
             // 
             // MvvmContext
             // 
@@ -48,45 +87,6 @@
             this.MvvmContext.ContainerControl = this;
             this.MvvmContext.ViewModelType = typeof(HangBreaker.ViewModels.MainViewModel);
             // 
-            // ButtonRestart
-            // 
-            ButtonRestart.Location = new System.Drawing.Point(50, 96);
-            ButtonRestart.Name = "ButtonRestart";
-            ButtonRestart.Size = new System.Drawing.Size(32, 32);
-            ButtonRestart.TabIndex = 2;
-            ButtonRestart.UseVisualStyleBackColor = true;
-            ButtonRestart.Visible = false;
-            // 
-            // ButtonStart
-            // 
-            ButtonStart.Location = new System.Drawing.Point(12, 96);
-            ButtonStart.Name = "ButtonStart";
-            ButtonStart.Size = new System.Drawing.Size(32, 32);
-            ButtonStart.TabIndex = 1;
-            ButtonStart.UseVisualStyleBackColor = true;
-            ButtonStart.Visible = false;
-            // 
-            // LabelDisplay
-            // 
-            LabelDisplay.Dock = System.Windows.Forms.DockStyle.Fill;
-            LabelDisplay.Font = new System.Drawing.Font("Comic Sans MS", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            LabelDisplay.ForeColor = System.Drawing.Color.Red;
-            LabelDisplay.Location = new System.Drawing.Point(0, 0);
-            LabelDisplay.Name = "LabelDisplay";
-            LabelDisplay.Size = new System.Drawing.Size(500, 140);
-            LabelDisplay.TabIndex = 0;
-            LabelDisplay.Text = "Overtime";
-            LabelDisplay.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // ButtonExit
-            // 
-            ButtonExit.Location = new System.Drawing.Point(456, 12);
-            ButtonExit.Name = "ButtonExit";
-            ButtonExit.Size = new System.Drawing.Size(32, 32);
-            ButtonExit.TabIndex = 3;
-            ButtonExit.UseVisualStyleBackColor = true;
-            ButtonExit.Visible = false;
-            // 
             // Timer
             // 
             Timer.Interval = 1000;
@@ -96,7 +96,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.AntiqueWhite;
-            this.ClientSize = new System.Drawing.Size(500, 140);
+            this.ClientSize = new System.Drawing.Size(319, 90);
             this.Controls.Add(ButtonExit);
             this.Controls.Add(ButtonRestart);
             this.Controls.Add(ButtonStart);
