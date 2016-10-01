d27b7e0bcccf73ab09f41589a007179ba09256b3
Astak <uranus@xaker.ru>
2016-10-01 20:52:20 +0300

    Added Timer

---
+++ HangBreaker/Controls/Timer.cs
@@ -0,0 +1,21 @@
+﻿using DevExpress.Utils.MVVM;
+using System;
+using System.Linq.Expressions;
+
+namespace HangBreaker.Controls {
+    public sealed class Timer :System.Windows.Forms.Timer, ISupportCommandBinding {
+        #region ISupportCommandBinding members
+        IDisposable ISupportCommandBinding.BindCommand(Expression<Action> commandSelector, object source, Func<object> queryCommandParameter) {
+            return CommandHelper.Bind(this, (that, action) => that.Tick += (s, e) => action(), (that, getState) => that.Enabled = getState(), commandSelector, source, queryCommandParameter);
+        }
+
+        IDisposable ISupportCommandBinding.BindCommand<T>(Expression<Action<T>> commandSelector, object source, Func<T> queryCommandParameter) {
+            return CommandHelper.Bind(this, (that, action) => that.Tick += (s, e) => action(), (that, getState) => that.Enabled = getState(), commandSelector, source, () => queryCommandParameter == null ? default(T) : queryCommandParameter());
+        }
+
+        IDisposable ISupportCommandBinding.BindCommand(object command, Func<object> queryCommandParameter) {
+            return CommandHelper.Bind(this, (that, action) => that.Tick += (s, e) => action(), (that, getState) => that.Enabled = getState(), command, queryCommandParameter);
+        }
+        #endregion
+    }
+}
