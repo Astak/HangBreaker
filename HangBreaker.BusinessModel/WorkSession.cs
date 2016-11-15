fe4a2822178ba9c567282632fe5932295b2df3ca
Astak <uranus@xaker.ru>
2016-11-15 10:07:17 +0300

    created business model

---
+++ HangBreaker.BusinessModel/WorkSession.cs
@@ -0,0 +1,30 @@
+﻿using DevExpress.Xpo;
+using System;
+
+namespace HangBreaker.BusinessModel {
+    public class WorkSession :XPObject {
+        public WorkSession(Session session) : base(session) { }
+
+        public string TicketID {
+            get { return GetPropertyValue<string>("TicketID"); }
+            set { SetPropertyValue<string>("TicketID", value); }
+        }
+
+        public WorkSessionStatus IntermediateStatus {
+            get { return GetPropertyValue<WorkSessionStatus>("IntermediateStatus"); }
+            set { SetPropertyValue<WorkSessionStatus>("IntermediateStatus", value); }
+        }
+
+        public WorkSessionStatus FinalStatus {
+            get { return GetPropertyValue<WorkSessionStatus>("FinalStatus"); }
+            set { SetPropertyValue<WorkSessionStatus>("FinalStatus", value); }
+        }
+
+        public TimeSpan Duration {
+            get { return GetPropertyValue<TimeSpan>("Duration"); }
+            set { SetPropertyValue<TimeSpan>("Duration", value); }
+        }
+    }
+
+    public enum WorkSessionStatus { NeedAnswer, NeedExample, NeedToDiscuss }
+}
