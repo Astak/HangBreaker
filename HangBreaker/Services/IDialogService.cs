b56070fb4e7555a4e649e5e4b7337e117ff45f1e
Astak <uranus@xaker.ru>
2016-11-27 16:33:32 +0300

    Added IDialogService interface

---
+++ HangBreaker/Services/IDialogService.cs
@@ -0,0 +1,8 @@
+﻿using System.Windows.Forms;
+
+namespace HangBreaker.Services {
+    public interface IDialogService {
+        DialogResult ShowDialog(UserControl view);
+        void HideDialog(DialogResult result);
+    }
+}
