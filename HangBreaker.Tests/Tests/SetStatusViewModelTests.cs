ba848d117f1e54c3d13c090bc376d6ffa0fb28b7
Astak <uranus@xaker.ru>
2016-11-28 21:16:32 +0300

    Start creating test: SetStatusUpdatesExistingRecordInSessionTableTest. Need learning parametrized view models

--- HangBreaker.Tests/Tests/SetStatusViewModelTests.cs
+++ HangBreaker.Tests/Tests/SetStatusViewModelTests.cs
@@ -1,4 +1,7 @@
-﻿using HangBreaker.BusinessModel;
+﻿using DevExpress.Mvvm;
+using DevExpress.Xpo;
+using HangBreaker.BusinessModel;
+using HangBreaker.Services;
 using HangBreaker.Tests.Views;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -16,6 +19,15 @@ namespace HangBreaker.Tests {
             var view = new TestSetStatusView();
             view.StatusControl.Value = WorkSessionStatus.NeedAnswer;
             Assert.IsTrue(view.OKAction.Enabled);
+        }
+
+        [TestMethod]
+        public void SetStatusUpdatesExistingRecordInSessionTableTest() {
+            var xpoService = ServiceContainer.Default.GetService<IXpoService>();
+            Session session = xpoService.GetSession();
+            var view = new TestSetStatusView();
+            view.StatusControl.Value = WorkSessionStatus.NeedAnswer;
+            view.OKAction.Execute();
         }
     }
 }
