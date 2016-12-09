38e340c719491fee0e20503764ceb0babe5a946c
Astak <uranus@xaker.ru>
2016-12-09 21:20:56 +0300

    Finished test: SetStatusUpdatesExistingRecordInSessionTable

--- HangBreaker/ViewModels/SetStatusViewModel.cs
+++ HangBreaker/ViewModels/SetStatusViewModel.cs
@@ -2,18 +2,12 @@
 using DevExpress.Mvvm.POCO;
 using HangBreaker.Services;
 using DevExpress.Xpo;
+using DevExpress.Mvvm;
 
 namespace HangBreaker.ViewModels {
-    public class SetStatusViewModel {
-        private readonly int WorkSessionId;
-        private readonly bool IsFinal;
-
-        public SetStatusViewModel(SetStatusViewModelParameters parameters) {
-            this.WorkSessionId = parameters.WorkSessionId;
-            this.IsFinal = parameters.IsFinal;
-        }
-
+    public class SetStatusViewModel :ISupportParameter {
         public virtual WorkSessionStatus? Status { get; set; }
+        protected virtual SetStatusViewModelParameters Parameter { get; set; }
 
         protected virtual IXpoService XpoService {
             get { throw new System.NotImplementedException(); }
@@ -21,8 +15,8 @@ namespace HangBreaker.ViewModels {
 
         public void Ok() {
             UnitOfWork uow = XpoService.GetUnitOfWork();
-            var workSession = uow.GetObjectByKey<WorkSession>(WorkSessionId);
-            if (IsFinal)
+            var workSession = uow.GetObjectByKey<WorkSession>(Parameter.WorkSessionId);
+            if (Parameter.IsFinal)
                 workSession.FinalStatus = Status.Value;
             else workSession.IntermediateStatus = Status.Value;
             uow.CommitChanges();
@@ -35,6 +29,12 @@ namespace HangBreaker.ViewModels {
         protected virtual void OnStatusChanged() {
             this.RaiseCanExecuteChanged(vm => vm.Ok());
         }
+        #region ISupportParameter
+        object ISupportParameter.Parameter {
+            get { return Parameter; }
+            set { Parameter = (SetStatusViewModelParameters)value; }
+        }
+        #endregion
     }
 
     public class SetStatusViewModelParameters {
