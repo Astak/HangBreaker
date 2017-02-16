8add086cb56b0015f110eecb3161336692859ad1
Astak <uranus@xaker.ru>
2017-02-16 22:42:15 +0300

    Added two tests

---
+++ HangBreaker.Tests.UI/UserControlDocumentAdapterTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Windows.Forms;
+using DevExpress.Utils.MVVM.Services;
+using HangBreaker.Documents;
+
+namespace HangBreaker.Tests.UI {
+    [TestClass]
+    public class UserControlDocumentAdapterTest {
+        [TestMethod]
+        public void ShowAddsToForm() {
+            var form = new Form();
+            var control = new Control();
+            IDocumentAdapter adapter = new UserControlDocumentAdapter(form);
+            adapter.Show(control);
+            Assert.AreEqual(form, control.Parent);
+        }
+
+        [TestMethod]
+        public void ShowDocksFill() {
+            var form = new Form();
+            var control = new Control();
+            IDocumentAdapter adapter = new UserControlDocumentAdapter(form);
+            adapter.Show(control);
+            Assert.AreEqual<DockStyle>(DockStyle.Fill, control.Dock);
+        }
+    }
+}
