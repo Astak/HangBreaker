c4c1c5a9985b91a5b718d12da5be9526cc3b0dc9
Astak <uranus@xaker.ru>
2017-02-16 22:42:15 +0300

    Added two tests

---
+++ HangBreaker/Services/Documents/UserControlDocumentAdapter.cs
@@ -0,0 +1,38 @@
+﻿using DevExpress.Utils.MVVM.Services;
+using System;
+using System.ComponentModel;
+using System.Windows.Forms;
+
+namespace HangBreaker.Documents {
+    public sealed class UserControlDocumentAdapter :IDocumentAdapter {
+        private Form form;
+
+        public UserControlDocumentAdapter(Form form) {
+            this.form = form;
+        }
+        #region IDocumentAdapter
+        void IDocumentAdapter.Close(Control control, bool force) {
+            throw new System.NotImplementedException();
+        }
+
+        event EventHandler IDocumentAdapter.Closed {
+            add { throw new System.NotImplementedException(); }
+            remove { throw new System.NotImplementedException(); }
+        }
+
+        event CancelEventHandler IDocumentAdapter.Closing {
+            add { throw new System.NotImplementedException(); }
+            remove { throw new System.NotImplementedException(); }
+        }
+
+        void IDocumentAdapter.Show(Control control) {
+            control.Dock = DockStyle.Fill;
+            control.Parent = form;
+        }
+
+        void IDisposable.Dispose() {
+            throw new System.NotImplementedException();
+        }
+        #endregion
+    }
+}
