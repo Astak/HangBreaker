5c6088f96bc11dd0b79686b4dda08d250cab081c
Astak <uranus@xaker.ru>
2017-02-19 18:45:19 +0300

    Moved MainForm content to View

--- HangBreaker/MainForm.Designer.cs
+++ HangBreaker/MainForm.Designer.cs
@@ -23,103 +23,20 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.components = new System.ComponentModel.Container();
-            System.Windows.Forms.Label LabelDisplay;
-            HangBreaker.Controls.Timer Timer;
-            this.ButtonExit = new System.Windows.Forms.Button();
-            this.ButtonRestart = new HangBreaker.Controls.Button();
-            this.ButtonStart = new HangBreaker.Controls.Button();
-            this.MvvmContext = new DevExpress.Utils.MVVM.MVVMContext(this.components);
-            LabelDisplay = new System.Windows.Forms.Label();
-            Timer = new HangBreaker.Controls.Timer();
-            ((System.ComponentModel.ISupportInitialize)(this.MvvmContext)).BeginInit();
-            this.SuspendLayout();
-            // 
-            // LabelDisplay
-            // 
-            LabelDisplay.Dock = System.Windows.Forms.DockStyle.Fill;
-            LabelDisplay.Font = new System.Drawing.Font("Comic Sans MS", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            LabelDisplay.ForeColor = System.Drawing.Color.Red;
-            LabelDisplay.Location = new System.Drawing.Point(0, 0);
-            LabelDisplay.Name = "LabelDisplay";
-            LabelDisplay.Size = new System.Drawing.Size(319, 90);
-            LabelDisplay.TabIndex = 0;
-            LabelDisplay.Text = "Overtime";
-            LabelDisplay.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            LabelDisplay.MouseMove += new System.Windows.Forms.MouseEventHandler(this.LabelDisplay_MouseMove);
-            // 
-            // ButtonExit
-            // 
-            this.ButtonExit.Location = new System.Drawing.Point(275, 12);
-            this.ButtonExit.Name = "ButtonExit";
-            this.ButtonExit.Size = new System.Drawing.Size(32, 32);
-            this.ButtonExit.TabIndex = 3;
-            this.ButtonExit.UseVisualStyleBackColor = true;
-            this.ButtonExit.Visible = false;
-            this.ButtonExit.Click += new System.EventHandler(this.ButtonExit_Click);
-            this.ButtonExit.Paint += new System.Windows.Forms.PaintEventHandler(this.ButtonExit_Paint);
-            // 
-            // ButtonRestart
-            // 
-            this.ButtonRestart.Location = new System.Drawing.Point(50, 46);
-            this.ButtonRestart.Name = "ButtonRestart";
-            this.ButtonRestart.Size = new System.Drawing.Size(32, 32);
-            this.ButtonRestart.TabIndex = 2;
-            this.ButtonRestart.UseVisualStyleBackColor = true;
-            this.ButtonRestart.Visible = false;
-            this.ButtonRestart.Paint += new System.Windows.Forms.PaintEventHandler(this.ButtonRestart_Paint);
-            // 
-            // ButtonStart
-            // 
-            this.ButtonStart.Location = new System.Drawing.Point(12, 46);
-            this.ButtonStart.Name = "ButtonStart";
-            this.ButtonStart.Size = new System.Drawing.Size(32, 32);
-            this.ButtonStart.TabIndex = 1;
-            this.ButtonStart.UseVisualStyleBackColor = true;
-            this.ButtonStart.Visible = false;
-            this.ButtonStart.Paint += new System.Windows.Forms.PaintEventHandler(this.ButtonStart_Paint);
-            // 
-            // Timer
-            // 
-            Timer.Interval = 1000;
-            // 
-            // MvvmContext
-            // 
-            this.MvvmContext.BindingExpressions.AddRange(new DevExpress.Utils.MVVM.BindingExpression[] {
-            DevExpress.Utils.MVVM.BindingExpression.CreateCommandBinding(typeof(HangBreaker.ViewModels.MainViewModel), "Restart", this.ButtonRestart),
-            DevExpress.Utils.MVVM.BindingExpression.CreateCommandBinding(typeof(HangBreaker.ViewModels.MainViewModel), "Start", this.ButtonStart),
-            DevExpress.Utils.MVVM.BindingExpression.CreatePropertyBinding(typeof(HangBreaker.ViewModels.MainViewModel), "DisplayText", LabelDisplay, "Text"),
-            DevExpress.Utils.MVVM.BindingExpression.CreateCommandBinding(typeof(HangBreaker.ViewModels.MainViewModel), "Tick", Timer)});
-            this.MvvmContext.ContainerControl = this;
-            this.MvvmContext.ViewModelType = typeof(HangBreaker.ViewModels.MainViewModel);
-            // 
-            // MainForm
-            // 
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.AntiqueWhite;
             this.ClientSize = new System.Drawing.Size(319, 90);
-            this.Controls.Add(this.ButtonExit);
-            this.Controls.Add(this.ButtonRestart);
-            this.Controls.Add(this.ButtonStart);
-            this.Controls.Add(LabelDisplay);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "MainForm";
             this.ShowInTaskbar = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
             this.Text = "Form1";
             this.TopMost = true;
-            ((System.ComponentModel.ISupportInitialize)(this.MvvmContext)).EndInit();
-            this.ResumeLayout(false);
-
         }
 
         #endregion
 
-        private DevExpress.Utils.MVVM.MVVMContext MvvmContext;
-        private System.Windows.Forms.Button ButtonExit;
-        private Controls.Button ButtonRestart;
-        private Controls.Button ButtonStart;
 
 
     }
--- HangBreaker/MainForm.cs
+++ HangBreaker/MainForm.cs
@@ -1,40 +1,23 @@
-﻿using DevExpress.Utils.MVVM.Services;
+﻿using DevExpress.Mvvm;
+using DevExpress.Utils.MVVM.Services;
 using HangBreaker.Documents;
-using HangBreaker.ViewModels;
-using System;
+using HangBreaker.Utils;
 using System.Drawing;
-using System.Drawing.Drawing2D;
 using System.Windows.Forms;
 
 namespace HangBreaker {
     public sealed partial class MainForm : Form, IDocumentAdapterFactory {
-        private Pen StartPen;
-        private Pen RestartPen;
-        private Pen ClosePen;
-
         public MainForm() {
             InitializeComponent();
-            InitializeBindings();
-            StartPen = new Pen(Brushes.Aqua, 5);
-            StartPen.StartCap = LineCap.Round;
-            StartPen.EndCap = LineCap.Round;
-            RestartPen = new Pen(Brushes.Aqua, 5);
-            RestartPen.StartCap = LineCap.Round;
-            RestartPen.EndCap = LineCap.ArrowAnchor;
-            ClosePen = new Pen(Brushes.WhiteSmoke, 4);
-            ClosePen.StartCap = LineCap.Triangle;
-            ClosePen.EndCap = LineCap.Triangle;
+            var service = (IDocumentManagerService)DocumentManagerService.Create(this);
+            ServiceContainer.Default.RegisterService(Constants.ServiceKey, service);
+            IDocument document = service.CreateDocument(Constants.MainViewName, null, null);
+            document.Show();
         }
 
         protected override void OnLoad(System.EventArgs e) {
             base.OnLoad(e);
             SetPosition();
-        }
-
-        private void InitializeBindings() {
-            if (DesignMode) return;
-            var api = MvvmContext.OfType<MainViewModel>();
-            api.SetBinding(this, d => d.Opacity, vm => vm.IsTransparent, val => val ? .5 : 1.0);
         }
 
         private void SetPosition() {
@@ -44,49 +27,6 @@ namespace HangBreaker {
                 );
         }
 
-        private void LabelDisplay_MouseMove(object sender, MouseEventArgs e) {
-            ButtonStart.Visible = ButtonStart.Enabled && ButtonStart.Bounds.Contains(e.Location);
-            ButtonRestart.Visible = ButtonRestart.Enabled && ButtonRestart.Bounds.Contains(e.Location);
-            ButtonExit.Visible = ButtonExit.Bounds.Contains(e.Location);
-        }
-
-        private void ButtonExit_Click(object sender, EventArgs e) {
-            Close();
-        }
-
-        private void ButtonStart_Paint(object sender, PaintEventArgs e) {
-            e.Graphics.FillRectangle(Brushes.AntiqueWhite, e.ClipRectangle);
-            double r = e.ClipRectangle.Width / 2.0;
-            int x = Convert.ToInt32(Math.Round(e.ClipRectangle.Left + r - r * Math.Sin(Math.PI / 3)));
-            double h = 3 * r / Math.Sqrt(3) / 2.0;
-            double ox = e.ClipRectangle.Top + r;
-            int y1 = Convert.ToInt32(Math.Round(ox - h));
-            int y2 = Convert.ToInt32(Math.Round(ox + h));
-            var points = new Point[] {
-                new Point(x, y1),
-                new Point(e.ClipRectangle.Right, Convert.ToInt32(Math.Round(ox))),
-                new Point(x, y2)
-            };
-            e.Graphics.FillPolygon(Brushes.Aqua, points);
-        }
-
-        private void ButtonRestart_Paint(object sender, PaintEventArgs e) {
-            e.Graphics.FillRectangle(Brushes.AntiqueWhite, e.ClipRectangle);
-            var arcBounds = e.ClipRectangle;
-            arcBounds.Inflate(-5, -5);
-            e.Graphics.DrawArc(RestartPen, arcBounds, 300, 300);
-        }
-
-        private void ButtonExit_Paint(object sender, PaintEventArgs e) {
-            e.Graphics.FillRectangle(Brushes.AntiqueWhite, e.ClipRectangle);
-            var buttonRect = e.ClipRectangle;
-            buttonRect.Inflate(-4, -4);
-            e.Graphics.FillEllipse(Brushes.IndianRed, buttonRect);
-            var centerPoint = new Point(e.ClipRectangle.Left + e.ClipRectangle.Width / 2, e.ClipRectangle.Top + e.ClipRectangle.Height / 2);
-            int crossSize = 5;
-            e.Graphics.DrawLine(ClosePen, centerPoint.X - crossSize, centerPoint.Y - crossSize, centerPoint.X + crossSize, centerPoint.Y + crossSize);
-            e.Graphics.DrawLine(ClosePen, centerPoint.X + crossSize, centerPoint.Y - crossSize, centerPoint.X - crossSize, centerPoint.Y + crossSize);
-        }
         #region IDocumentAdapterFactory
         IDocumentAdapter IDocumentAdapterFactory.Create() => new UserControlDocumentAdapter(this);
         #endregion
